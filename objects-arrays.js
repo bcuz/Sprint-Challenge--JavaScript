842884f7f7f25484a413c3cb6050de8928a09608
adam <bcuz@users.noreply.github.com>
2019-01-26 15:00:56 -0500

    mvp complete.

--- objects-arrays.js
+++ objects-arrays.js
@@ -132,9 +132,9 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
-for (const animal of zooAnimals) {
-  animalNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}`)
-}
+
+zooAnimals.forEach(animal => animalNames.push(`Name: ${animal.animal_name}, Scientific: ${animal.scientific_name}`))
+
 console.log(animalNames);
 
 /* Request 2: .map()    
